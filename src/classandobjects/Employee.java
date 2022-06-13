72e31900eccdd655b3132af8b96d627b7cbd12ea
palak-rane <107403370+palak-rane@users.noreply.github.com>
2022-06-13 14:35:30 +0100

    Employee details

--- src/classandobjects/Employee.java
+++ src/classandobjects/Employee.java
@@ -1,4 +1,26 @@
 package classandobjects;
 
 public class Employee {
+    String empName;
+    int empId;
+    float empSalary;
+    String empAddress;
+
+    public Employee(String empName, int empId, float empSalary, String empAddress ){
+      this.empName = empName;
+      this.empId= empId;
+      this.empSalary =empSalary;
+      this.empAddress= empAddress;
+    }
+
+    public static void main(String[] args) {
+        Employee palak =new Employee("Palak", 1, 10000, "Harrow");
+        Employee vinod =new Employee("Vinod", 2, 200000, "Wembley");
+        Employee janhavi =new Employee("Janhavi", 3, 3000, "London");
+        Employee anvi =new Employee("Anvi", 4, 10000, "Harrow");
+
+        System.out.println(palak.empAddress + " " + palak.empSalary + " " + palak.empId +" " + palak.empName);
+
+    }
+
 }
