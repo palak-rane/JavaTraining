a38a6c5ffb9619ff97a1d13ddde520b4dfc743b9
palak-rane <107403370+palak-rane@users.noreply.github.com>
2022-06-27 17:55:27 +0100

    Casting

---
+++ src/gettingstarted/Casting.java
@@ -0,0 +1,16 @@
+package gettingstarted;
+
+public class Casting {
+    public static void main(String[] args) {
+        String s1 = "11";
+        int n1 = 22;
+        int sum = Integer.parseInt(s1) +n1 ;
+        System.out.println(sum);
+
+        String str1 = "Palak";
+        int n2 = 33;
+
+        System.out.println(str1.concat(String.valueOf(n2)));
+
+    }
+}
